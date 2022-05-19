9ecbc4c94a8f59c5ec18138567dc35dc3dd219ca
Saif <sma1043@rit.edu>
2022-05-19 20:16:36 +0400

    Added: Process Duration

--- code/camera.py
+++ code/camera.py
@@ -1,6 +1,5 @@
-import cv2
 from PIL import Image
-import os
+import cv2
 
 #Set the camera to the primary camera of hardware
 cap = cv2.VideoCapture(0)
@@ -8,19 +7,23 @@ cap = cv2.VideoCapture(0)
 #Variable for storing the image
 Image = None
 
-#returns the latest image from the camera
+#------------------- IMPORTANT ---------------------
+# IF YOU WANT TO PROCCESS AN IMAGE SIMPLY FOLLOW THE EXAMPLE BELOW
+# 
+# EXAMPLE:
+# import camera
+# image = camera.getImage() 
 def getImage():
     return Image
 
-#Capture the image 
+#Capture the image (DONT USE THIS)
 def updateImage():
     global Image
 
     ret, frame = cap.read()
     if ret:
-        ret, frame = cap.read()
+
         rawImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        
         Image = cv2.flip(rawImage, 1)
     else:
         print("ERROR: NOT ABLE TO RETRIVE VIDEO DATA...")
--- code/gui.py
+++ code/gui.py
@@ -3,22 +3,28 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import camera
 import score
+import time
+
+duration = 0
 
 #Construction for the window application
 class Gui(QWidget):
     def __init__(self):
         super(Gui, self).__init__()
 
+        #setting up labels and GUI using Pyqt
         self.setWindowTitle("Football Referee")
         self.VBL = QVBoxLayout()
 
         self.FeedLabel = QLabel()
         self.ScoreALabel = QLabel(("Team A: " + str(score.getScore('a'))), self)
         self.ScoreBLabel = QLabel(("Team B: " + str(score.getScore('b'))), self)
+        self.ProcessDur = QLabel(("Proccess Duration: " + str(round(duration)) + "ms"), self)
 
         self.VBL.addWidget(self.FeedLabel)
         self.VBL.addWidget(self.ScoreALabel)
         self.VBL.addWidget(self.ScoreBLabel)
+        self.VBL.addWidget(self.ProcessDur)
         
         self.ResetButton = QPushButton("Reset")
         self.ResetButton.clicked.connect(score.resetScore)
@@ -29,32 +35,51 @@ class Gui(QWidget):
         self.Worker1.ImageUpdate.connect(self.ImageUpdateSlot)
         self.setLayout(self.VBL)
 
+    #Calls when its time to update the gui
     def ImageUpdateSlot(self, image):
         self.FeedLabel.setPixmap(QPixmap.fromImage(image))
         self.ScoreALabel.setText(("Team A: " + str(score.getScore('a'))))
         self.ScoreBLabel.setText(("Team B: " + str(score.getScore('b'))))
+        self.ProcessDur.setText(("Proccess Duration: " + str(round(duration)) + "ms"))
 
 
 
 #Capture video from camera is being dealt from a seperate "thread" than the main thread to prevent freeze.
 class Worker1(QThread):
+    global duration
 
     ImageUpdate = pyqtSignal(QImage)
 
     def run(self):
 
         self.ThreadActive = True
-        while self.ThreadActive:
 
+        # "Main loop" - computer vision should be moved to a different loop
+        while self.ThreadActive:
+            global duration
+
+            #get the current time in ns to calculate duration for proccessing
+            t = time.time_ns() 
+
+            #updating image from webcam
             camera.updateImage()
+
+            #computer vision should be proccessed here (for now)
+
+            #fetching image to display on the gui
             image = camera.getImage()
 
+            #image being converted to QImage for displaying (for Pyqt to render it)
             ConvertToQtFormat = QImage(image.data, image.shape[1], image.shape[0], QImage.Format_RGB888)
             Pic = ConvertToQtFormat.scaled(1080, 720, Qt.KeepAspectRatio)
             self.ImageUpdate.emit(Pic)
 
+            #get current time and subtract it by t
+            duration = time.time_ns() - t
+
+            #convert duration from ns to milisec
+            duration = duration / 1000000
+
     def stop(self):
         self.ThreadActive = False
         self.quit()
-
-
